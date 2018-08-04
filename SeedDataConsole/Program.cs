011b3bbe291c1e8c29b028b04d709ce5e415779d
zfking <zfking@gmail.com>
2018-08-04 22:14:06 +0800

    增加循环测试

--- SeedDataConsole/Program.cs
+++ SeedDataConsole/Program.cs
@@ -16,6 +16,10 @@ namespace SeedDataConsole
         //command in package manager console
         // Scaffold-DbContext "Server=.;Database=XLDDSMTest;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context DSMContext
         public static string ConnectionString = "Server=.;Database=XLDDSMTest2;Trusted_Connection=True;";
+
+        public static string PrefixName = "test";
+        public static DateTime StartDate = new DateTime(1900, 1, 1);
+
         static void Main(string[] args)
         {
             Console.WriteLine("是否删除所有数据，然后重新插入数据？yes/no");
@@ -44,24 +48,130 @@ namespace SeedDataConsole
 
             }
 
+            int insertSensorCnt = 9899;
+            int insertDataCntPerSensor = 87600;
             if (cnt == 0)
             {
                 Stopwatch stop = new Stopwatch();
                 stop.Start();
-                int insertSensorCnt = 9899;
-                int insertDataCntPerSersor = 87600;
-                seedSensor_Data(insertSensorCnt, insertDataCntPerSersor);
+
+                seedSensor_Data(insertSensorCnt, insertDataCntPerSensor);
                 stop.Stop();
 
                 Console.WriteLine(string.Format("插入 {0:N0} 条数据，用时 {1:N0} 毫秒。",
-                   (long)insertSensorCnt * insertDataCntPerSersor, stop.ElapsedMilliseconds));
+                   (long)insertSensorCnt * insertDataCntPerSensor, stop.ElapsedMilliseconds));
             }
 
             cnt = dbcontext.SensorInfo.Count();
 
             Console.WriteLine("测点数量: " + cnt);
+            ProjectInfo project = getFirstProject(dbcontext);
+            ConsoleKeyInfo input;
+            Console.WriteLine("单测点按时间分别查询测试：");
+            do
+            {
+                testQuerySingle(dbcontext, project, insertSensorCnt);
+                Console.Write("输入n退出当前测试循环，");
+                input = Console.ReadKey();
+                Console.WriteLine();
+            } while (input.KeyChar != 'n');
+
+            Console.WriteLine("20测点按时间分别查询测试：");
+            do
+            {
+                testQueryMultiply(dbcontext, project, insertSensorCnt);
+                Console.Write("输入n退出当前测试循环，");
+                input = Console.ReadKey();
+                Console.WriteLine();
+            } while (input.KeyChar != 'n');
+
 
             prepareQuit();
+        }
+
+        private static void testQueryMultiply(DSMContext db, ProjectInfo project, int insertSensorCnt)
+        {
+
+            //clearDbCache(db);
+
+
+            List<Guid> sensorIdList = getRandom20SensorIdList(db, project, insertSensorCnt);
+
+
+
+
+            var query = from i in db.SensorDataOrigin
+                        where sensorIdList.Contains(i.SensorId) && i.MeaTime > StartDate.AddYears(2) && i.MeaTime < StartDate.AddYears(2).AddDays(15)
+                        select i;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = query.ToArray();
+            watch.Stop();
+
+            Console.WriteLine(string.Format("随机查询并返回20测点多个数据，返回数据{0}条, 耗时 {1:N0} ms。",
+                result.Length, watch.ElapsedMilliseconds));
+
+
+        }
+
+        private static void testQuerySingle(DSMContext db, ProjectInfo project, int insertSensorCnt)
+        {
+
+            //clearDbCache(db);
+            SensorInfo sensor = getRandomSensor(db, project, insertSensorCnt);
+
+
+            var query = from i in db.SensorDataOrigin
+                        where i.SensorId == sensor.Id && i.MeaTime > StartDate.AddYears(2) && i.MeaTime < StartDate.AddYears(2).AddMonths(3)
+                        select i;
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = query.ToArray();
+            watch.Stop();
+
+            Console.WriteLine(string.Format("随机查询并返回单个测点半年数据，返回数据{0}条, 耗时 {1:N0} ms。sensor code: {2}",
+                result.Length, watch.ElapsedMilliseconds, sensor.SensorCode));
+
+
+        }
+
+        private static SensorInfo getRandomSensor(DSMContext db, ProjectInfo project, int insertSensorCnt)
+        {
+            Random r = new Random();
+            int index = r.Next(insertSensorCnt);
+            string name = PrefixName + index.ToString("d5");
+            var sensor = db.SensorInfo.First(s => s.ProjectId == project.Id && s.SensorCode == name);
+
+            return sensor;
+
+
+        }
+
+        private static List<Guid> getRandom20SensorIdList(DSMContext db, ProjectInfo project, int insertSensorCnt)
+        {
+            List<string> codeList = new List<string>();
+            for (int i = 0; i < 20; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(insertSensorCnt);
+                string name = PrefixName + index.ToString("d5");
+                codeList.Add(name);
+            }
+            var query = from i in db.SensorInfo
+                        where i.ProjectId == project.Id && codeList.Contains(i.SensorCode)
+                        orderby i.Id
+                        select i.Id;
+            var result = query.ToList();
+            return result;
+
+
+        }
+
+        private static void clearDbCache(DSMContext db)
+        {
+            string sql = @"CHECKPOINT;
+DBCC DROPCLEANBUFFERS;";
+            db.Database.ExecuteSqlCommand(sql);
         }
 
         private static ProjectInfo getFirstProject(DSMContext dbcontext)
@@ -83,7 +193,7 @@ namespace SeedDataConsole
 
         private static void prepareQuit()
         {
-            Console.WriteLine("回车 退出");
+            Console.WriteLine("回车 退出程序");
 
             Console.ReadLine();
 
@@ -127,7 +237,6 @@ namespace SeedDataConsole
             //sensorInfoTable.Columns.Add("SensorCode", typeof(string));
 
 
-            DateTime startDate = new DateTime(1900, 1, 1);
 
 
             ProjectInfo project = getFirstProject(dbcontext);
@@ -137,7 +246,7 @@ namespace SeedDataConsole
             {
                 //Console.WriteLine(string.Format("inserting No. {0} Sensor info ", i + 1));
 
-                string sensorCode = "test" + i.ToString("d5");
+                string sensorCode = PrefixName + i.ToString("d5");
                 SensorInfo sen = new SensorInfo();
                 sen.Id = Guid.NewGuid();
                 sen.SensorCode = sensorCode;
@@ -166,7 +275,7 @@ namespace SeedDataConsole
 
                     var item = new SensorDataOrigin();
                     item.SensorId = senItem.Id;
-                    item.MeaTime = startDate.AddHours(0.5 * j);
+                    item.MeaTime = StartDate.AddHours(0.5 * j);
                     item.MeaValue1 = j;
                     item.MeaValue2 = j;
                     item.MeaValue3 = j;
