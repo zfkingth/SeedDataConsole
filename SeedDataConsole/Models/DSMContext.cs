c09bf945a359aefe70971a69a8174b3d3ab62480
zfking <zfking@gmail.com>
2018-08-06 23:28:19 +0800

    temp

--- SeedDataConsole/Models/DSMContext.cs
+++ SeedDataConsole/Models/DSMContext.cs
@@ -24,7 +24,7 @@ namespace SeedDataConsole.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=.;Database=DSM3;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("Server=.;Database=IOM;Trusted_Connection=True;");
             }
         }
 
